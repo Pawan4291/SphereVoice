5d9d18353674a89ddc88ba4dce8720bff5214b9a
Pawan4291 <pawan429014@gmail.com>
2026-07-03 14:06:56 +0530

    fix schedule payment

--- api/cron-execute.ts
+++ api/cron-execute.ts
@@ -1,5 +1,5 @@
 import { Redis } from '@upstash/redis';
-import { getAstridWallet } from './_astrid';
+import { getAstridWallet } from './_astrid.js';
 
 const redis = new Redis({ url: process.env.KV_REST_API_URL!, token: process.env.KV_REST_API_TOKEN! });
 
--- api/test-astrid.ts
+++ api/test-astrid.ts
@@ -1,4 +1,4 @@
-import { getAstridWallet } from './_astrid';
+import { getAstridWallet } from './_astrid.js';
 export default async function handler(req: any, res: any) {
   const sphere = await getAstridWallet();
   res.json({ address: sphere.identity?.directAddress, nametag: sphere.identity?.nametag });
